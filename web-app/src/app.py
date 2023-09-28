3135904547e9209d2383040bad9a9da0f0e330a2
mikaellafs <mikaellaferreira0@gmail.com>
2023-09-27 21:08:14 -0300

    Get user preferences in web app

--- web-app/src/app.py
+++ web-app/src/app.py
@@ -1,17 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
 
 app = Flask(__name__)
 
+# List of locations available
+locations = ["Sao Paulo", "Rio de Janeiro", "Vitória"]
+
+# List of weather conditions
+weather_conditions = {
+    "sunny": "Sunny",
+    "rainy": "Rainy",
+    "cloudy": "Cloudy",
+    "windy": "Windy"
+}
+
+
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    return render_template("index.html", locations=locations, weather_conditions=weather_conditions)
+
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    location = request.form.get("location")
+    start_date = request.form.get("start_date")
+    end_date = request.form.get("end_date")
+    weather_condition = request.form.get("weather_condition")
+
+    return f"What is the most likely day to be {weather_condition} between {start_date} and {end_date} in {location}? Wait..."
