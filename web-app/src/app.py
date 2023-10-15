31708a91388015f3b1200acf4ee1c6e8b150ca76
mikaellafs <mikaellaferreira0@gmail.com>
2023-10-15 12:13:53 -0300

    Get request from user, send to db and push to queue

--- web-app/src/app.py
+++ web-app/src/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, request,render_template
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from flask import Flask, request,render_template, jsonify
+from models.request import Request, save_request_to_db
+from rabbit.push import push_to_rabbitmq
 
 app = Flask(__name__)
 
 # List of locations available
-locations = ["Sao Paulo", "Rio de Janeiro", "Vitória"]
+locations = ["New York", "Los Angeles", "London", "Paris", "Tokyo", "Sydney", "Rio de Janeiro"]
 
 # List of weather conditions
 weather_conditions = {
@@ -13,17 +19,25 @@ weather_conditions = {
     "windy": "Windy"
 }
 
-
 @app.route("/")
 def main():
     return render_template("index.html", locations=locations, weather_conditions=weather_conditions)
 
-
-@app.route("/echo_user_input", methods=["POST"])
+@app.route("/send_request", methods=["POST"])
 def echo_input():
+    user = request.form.get("username")
     location = request.form.get("location")
     start_date = request.form.get("start_date")
     end_date = request.form.get("end_date")
     weather_condition = request.form.get("weather_condition")
 
+    req = Request(user=user, location=location, start_date=start_date, end_date=end_date)
+    try:
+        save_request_to_db(req)
+        push_to_rabbitmq(req.id, location=location, start_date=start_date, end_date=end_date)
+    except Exception as e:
+        return jsonify(error=e.args[0]), 500
+
     return f"What is the most likely day to be {weather_condition} between {start_date} and {end_date} in {location}? Wait..."
+
+app.run()
---
+++ web-app/src/rabbit/push.py
@@ -0,0 +1,25 @@
+import pika
+import os
+import json
+
+def push_to_rabbitmq(user, location, start_date, end_date):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host= os.getenv("RABBIT_HOST"),
+        port= os.getenv("RABBIT_PORT"),  
+        # credentials=pika.PlainCredentials('username', 'password')
+        ))
+    channel = connection.channel()
+    channel.queue_declare(queue=os.getenv("REQUESTS_QUEUE"))
+    channel.basic_publish(exchange=os.getenv("REQUESTS_EXCHANGE"), routing_key='42', body=json.dumps({
+        "request_id": user,
+        "location": location,
+        "start_date": start_date,
+        "end_date": end_date
+    }))
+    print(json.dumps({
+        "request_id": user,
+        "location": location,
+        "start_date": start_date,
+        "end_date": end_date
+    }))
+    connection.close()
