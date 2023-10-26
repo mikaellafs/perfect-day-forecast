bea39efb08094c900803c2739fadeac724538223
mikaellafs <mikaellaferreira0@gmail.com>
2023-10-26 14:48:28 -0300

    Implement weather analyzer and process requests

--- services/applications/data-analyzer/src/main/kotlin/com/example/perfectdayforecast/app/App.kt
+++ services/applications/data-analyzer/src/main/kotlin/com/example/perfectdayforecast/app/App.kt
@@ -1,14 +1,21 @@
 package com.example.perfectdayforecast.app
 
 import com.example.perfectdayforecast.analyzer.PerfectDayRequest
+import com.example.perfectdayforecast.analyzer.SQLDataGateway
 import com.example.perfectdayforecast.analyzer.WeatherAnalyzer
 import com.example.perfectdayforecast.rabbitsupport.*
+import com.example.perfectdayforecast.analyzer.DataGateway
 import java.net.URI
 
 import kotlinx.coroutines.CoroutineScope
 import com.rabbitmq.client.ConnectionFactory
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+
+class App
+
+private val logger = LoggerFactory.getLogger(App::class.java)
 
 fun main(): Unit = runBlocking {
     val rabbitUrl = System.getenv("RABBIT_URL")?.let(::URI)
@@ -17,9 +24,13 @@ fun main(): Unit = runBlocking {
         ?: throw RuntimeException("Please set the RABBIT_ANALYZER_QUEUE environment variable")
     val weatherAnalyzerExchangeName =System.getenv("RABBIT_ANALYZER_EXCHANGE")
         ?: throw RuntimeException("Please set the RABBIT_ANALYZER_EXCHANGE environment variable")
+    val databaseUrl = System.getenv("DATABASE_URL")
+        ?: throw RuntimeException("Please set the DATABASE_URL environment variable")
+        
+    val dbConfig = DatabaseConfiguration(databaseUrl)
 
     val connectionFactory = buildConnectionFactory(rabbitUrl)
-//    val dataGateway: DataGateway =
+    val dataGateway: DataGateway = SQLDataGateway(dbConfig.db)
 
     val weatherAnalyzerExchange = RabbitExchange(
         name = weatherAnalyzerExchangeName,
@@ -31,28 +42,31 @@ fun main(): Unit = runBlocking {
     connectionFactory.declareAndBind(exchange = weatherAnalyzerExchange, queue = weatherAnalyzerQueue)
 
     listenForWeatherAnalysisRequests(
-//        dataGateway,
+        dataGateway,
         connectionFactory,
         weatherAnalyzerQueue
     )
 }
 
 fun CoroutineScope.listenForWeatherAnalysisRequests(
-//    dataGateway: WeatherDataGateway,
+    dataGateway: DataGateway,
     connectionFactory: ConnectionFactory,
     weatherAnalysisQueue: RabbitQueue,
 ) {
-//    val weatherAnalyzer = WeatherAnalyzer(dataGateway)
-
+    val weatherAnalyzer = WeatherAnalyzer(dataGateway)
     launch {
-        println("listening for analysis requests")
+        logger.info("listening for analysis requests")
         val channel = connectionFactory.newConnection().createChannel()
 
         listen(queue = weatherAnalysisQueue, channel = channel) { message ->
-            val request = PerfectDayRequest.fromJson(message)
-//            logger.debug("received weather forecast request for days from {} to {}", request.startDate, request.endDate)
+            try {
+                val request = PerfectDayRequest.fromJson(message)
+                logger.info("received weather analysis request id ${request.requestId} for a ${request.weatherPreference.name} weather condition")
 
-//            weatherAnalyzer.analyzeRequest(request)
+                weatherAnalyzer.analyzeRequest(request)
+            } catch (e: Exception) {
+                logger.error("Error occurred when processing message \"$message\"", e)
+            }
         }
     }
 }
--- services/components/data-analyzer/src/main/kotlin/com/example/perfectdayforecast/analyzer/WeatherAnalyzer.kt
+++ services/components/data-analyzer/src/main/kotlin/com/example/perfectdayforecast/analyzer/WeatherAnalyzer.kt
@@ -2,6 +2,15 @@ package com.example.perfectdayforecast.analyzer
 
 class WeatherAnalyzer(private val dataGateway: DataGateway) {
     fun analyzeRequest(request: PerfectDayRequest) {
+        // Get best day
+        var bestDay: WeatherForecast? = null
+        for (day in request.days) {
+            if (day.weathercode == request.weatherPreference) {
+                bestDay = day
+            }
+        }
 
+        // Update request
+        dataGateway.updateRequest(request.requestId, RequestStatus.DONE, bestDay)
     }
 }
